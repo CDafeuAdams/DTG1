2589fff48aae8b4944bebeacbcf2d8e29449f9be
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-11-06 15:45:30 -0600

    defined list generator function

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def greet_user():
     print("Hello Traveler")
 traveler_name = input("What is your name?")
 print("Hello " + traveler_name)
+
+def list_generator():
+    texas_locations = ["Houston", "Austin", "San Antonio", "El Paso", "Waco", "Arlington", "Corpus Christi", "Dallas", "Fort Worth", "Plano", "Laredo"]
+    texas_restaurants = ["Mo Better Brews", "Torchy's", "Cappy's", "Zino's Greek", "Magnolia Table", "Pappadeaux Seafood Kitchen", "Saltwater Grill", "Rodeo Goat", "Enchilada's Ole", "Grace", "Tabernilla"]
+    transportations = ["motorcycle", "horse", "car", "train", "monster truck", "SUV", "helicopter", "boat", "hot air balloon"]
+    texas_entertainments = ["to the Rodeo", "Fishing", "to the Beach", "to Six Flags", "to the River Walk", "to the Space Center", "to the Natural Bridge Caverns", "to the Museum", "to the Van Gogh Exhibit", "to the Comedy Club", "to the Aquarium", "to the Zoo", "to the National Park"]
+    return [texas_locations, texas_restaurants, transportations, texas_entertainments]
